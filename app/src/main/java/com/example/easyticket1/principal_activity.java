e8e8217f911362897a509262f375127b69e8ff8a
MonicaTriana <70044184+MonicaTriana@users.noreply.github.com>
2021-06-21 14:53:10 -0500

    primer commit

---
+++ app/src/main/java/com/example/easyticket1/principal_activity.java
@@ -0,0 +1,35 @@
+package com.example.easyticket1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class principal_activity extends AppCompatActivity {
+    Button btn, btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        btn1 = findViewById(R.id.btn_crear_cuenta);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), crear_cuenta_activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn = findViewById(R.id.btn_iniciar_sesion);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), menu_activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
